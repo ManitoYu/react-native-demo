09e4d8072ecb04cc25a2c7ff9ae70b1b9fd66024
ManitoYu <412950798@qq.com>
2016-04-23 16:51:39 +0800

    修改抽屉导航列表项样式

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -2,24 +2,33 @@ import React, {
   Component,
   PropTypes,
   View,
-  Text
+  Text,
+  Image
 } from 'react-native'
 
 import {
   Divider
 } from '../components'
 
+const Icon = require('react-native-vector-icons/MaterialIcons')
+
 import List from '../containers/List'
 
 const styles = {
   drawerHeader: {
-    height: 120,
     backgroundColor: '#2196F3',
-    marginBottom: 8
-  },
-  drawerBody: {
+    marginBottom: 8,
     padding: 16
   },
+  drawerBody: {
+
+  },
+  navItem: {
+   flex: 1,
+   flexDirection: 'row',
+   padding: 16,
+   alignItems: 'center'
+ },
   drawerFooter: {
 
   }
@@ -31,9 +40,20 @@ export default class Drawer extends Component {
 
     return (
       <View>
-        <View style={styles.drawerHeader}></View>
+        <View style={styles.drawerHeader}>
+          <Image
+            style={{ width: 60, height: 60, borderRadius: 30 }}
+            source={{ uri: 'http://yucong.oss-cn-shanghai.aliyuncs.com/yinmu.png?Expires=1461310833&OSSAccessKeyId=kziCCZuOkMBVYp8L&Signature=HJj7PeLOsUxDLBB/msudVhfgIVY%3D'}} />
+        </View>
         <View style={styles.drawerBody}>
-          <Text onPress={() => navigator.push({ name: 'List', component: List })}>List</Text>
+          <View style={styles.navItem}>
+            <View style={{ marginRight: 32 }}><Icon name="format-list-bulleted" size={24} color="#0091EA" /></View>
+            <Text onPress={() => navigator.push({ name: 'List', component: List })}>首页</Text>
+          </View>
+          <View style={styles.navItem}>
+            <View style={{ marginRight: 32 }}><Icon name="explore" size={24} color="#00C853" /></View>
+            <Text onPress={() => navigator.push({ name: 'List', component: List })}>发现</Text>
+          </View>
         </View>
         <Divider />
         <View style={styles.drawFooter}></View>
